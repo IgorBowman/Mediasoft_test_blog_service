cb2bebd3f8f582c2391fd5d56f5d23adfa5a6eab
IgorBowman <jitser26@gmail.com>
2022-12-01 21:05:07 +0400

    add user register view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import CreateCustomUserSerializer
+from .models import CustomUser
+
+
+class RegistrationAPI(GenericAPIView):
+    serializer_class = CreateCustomUserSerializer
+    queryset = CustomUser.objects.all()
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response({
+            "user": CreateCustomUserSerializer(
+                user,
+                context=self.get_serializer_context()
+            ).data
+        })
