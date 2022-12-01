9ea7e82f6726cc977f070d063957322fc77fa016
IgorBowman <jitser26@gmail.com>
2022-12-01 20:15:45 +0400

    a little change serializer for users

--- users/serializers.py
+++ users/serializers.py
@@ -6,17 +6,11 @@ from .models import CustomUser
 class CreateCustomUserSerializer(serializers.ModelSerializer):
     """ Serializer for create new user. """
 
-    password = serializers.CharField(
-        max_length=128,
-        min_length=8,
-        write_only=True
-    )
-
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'password', ]
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        # Использовать метод create_user, который мы
-        # написали ранее, для создания нового пользователя.
-        return CustomUser.objects.create_user(**validated_data)
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
