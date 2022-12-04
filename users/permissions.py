19ce64828fcfaa3032e438726d3aadfe4fc940a8
IgorBowman <jitser26@gmail.com>
2022-12-04 21:22:59 +0400

    add admin or read only permission

--- users/permissions.py
+++ users/permissions.py
@@ -40,3 +40,13 @@ class IsUserAuthorOrBlogOwner(permissions.BasePermission):
                     or obj.owner == request.user:
                 return True
             return False
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """Доступ для администратора или для чтения"""
+
+    def has_permission(self, request, view):
+        if (request.method in permissions.SAFE_METHODS) \
+                or (request.user and request.user.is_staff):
+            return True
+        return False
