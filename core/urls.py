f542b902ea21cf06d959bf7c780936cd56d5c5a7
IgorBowman <jitser26@gmail.com>
2022-12-04 21:54:32 +0400

    include urls from blogs.urls

--- core/urls.py
+++ core/urls.py
@@ -11,10 +11,13 @@ from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('users.urls')),
+
+    path('auth/', include('users.urls')),
+    path('api/', include('blogs.urls')),
 ]
 
 urlpatterns += doc_urls
