86b44305aa4b70daf550b92089c4250fd6cd7878
IgorBowman <jitser26@gmail.com>
2022-12-05 16:33:59 +0400

    created MixinFilterData for  filter_backend

---
+++ blog/mixins.py
@@ -0,0 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+
+class MixinFilterData:
+    filter_backend = (
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
+    )
