fd6fc70f69e6a2e78b82b7f20f6a3233c0edeb84
IgorBowman <jitser26@gmail.com>
2022-12-05 18:49:57 +0400

    add paginationclass, makemigrations

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,3 @@
-import os
 import environs
 from pathlib import Path
 
@@ -127,10 +126,12 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
